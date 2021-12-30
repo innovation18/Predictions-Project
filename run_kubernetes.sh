0151c643b88ad5a1258329400937edf3ab6cc489
Hanish Arora <61754555+innovation18@users.noreply.github.com>
2021-12-30 13:22:35 +0530

    Added - Kubernetes Setup

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=hanisharora/predictions-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment predictions-app --image=hanisharora/predictions-app
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/predictions-app 8000:80
 
