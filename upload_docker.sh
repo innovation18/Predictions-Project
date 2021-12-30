3569c2abaca9bd877a737153386c6c27dba7fcc3
Hanish Arora <61754555+innovation18@users.noreply.github.com>
2021-12-30 12:43:40 +0530

    Pushing Repo to DockerHub

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=hanisharora/predictions-app
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: $DOCKERPATH"
+docker login --username=hanisharora
+docker tag predictions-app hanisharora/predictions-app 
 
 # Step 3:
 # Push image to a docker repository
+docker push hanisharora/predictions-app
